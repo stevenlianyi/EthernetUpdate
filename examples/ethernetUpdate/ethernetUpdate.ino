23cf7f8553289941404d5a3f5b930c423c4b03a6
Steven Lian <steven.lian@gmail.com>
2021-01-04 14:55:31 +0800

    Update ethernetUpdate.ino

--- examples/ethernetUpdate/ethernetUpdate.ino
+++ examples/ethernetUpdate/ethernetUpdate.ino
@@ -1,7 +1,25 @@
 /**
-   httpUpdate.ino
+  ethernetUpdate.ino
 
-    Created on: 27.11.2015
+  Created on: 2020-12-21
+  
+  by Steven Lian
+  
+  ESP32 Ethernet upgrade.
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 */
 
@@ -10,10 +28,20 @@
 //#include <WiFi.h>
 //#include <WiFiMulti.h>
 
-#include <EthernetHTTPClient.h>
-#include <EthernetHTTPUpdate.h>
+#include "Ethernet.h"
 
-WiFiMulti WiFiMulti;
+#include "EthernetHTTPClient.h"
+#include "EthernetUpdate.h"
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192, 168, 0, 177);
+IPAddress myDns(192, 168, 0, 1);
+
+char updateUrl[] = "http://app.iottest.online/upgrade/helloword.ino.bin";
 
 void setup() {
 
@@ -30,17 +58,40 @@ void setup() {
     delay(1000);
   }
 
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("SSID", "PASSWORD");
+  //WiFi.mode(WIFI_STA);
+  //WiFiMulti.addAP("SSID", "PASSWORD");
 
+  // start the Ethernet connection:
+  Serial.println("Initialize Ethernet with DHCP:");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1); // do nothing, no point running without Ethernet hardware
+      }
+    }
+    if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip, myDns);
+  } else {
+    Serial.print("  DHCP assigned IP ");
+    Serial.println(Ethernet.localIP());
+  }
 
 }
 
 void loop() {
   // wait for WiFi connection
-  if ((WiFiMulti.run() == WL_CONNECTED)) {
+  //if ((WiFiMulti.run() == WL_CONNECTED))
+  {
 
-    WiFiClient client;
+    //WiFiClient client;
+    EthernetClient client;
+    client.setTimeout(10000);
 
     // The line below is optional. It can be used to blink the LED on the board during flashing
     // The LED will be on during download of one buffer of data from the network. The LED will
@@ -50,13 +101,13 @@ void loop() {
     // value is used to put the LED on. If the LED is on with HIGH, that value should be passed
     // httpUpdate.setLedPin(LED_BUILTIN, LOW);
 
-    t_httpUpdate_return ret = httpUpdate.update(client, "http://server/file.bin");
+    t_httpUpdate_return ret = ethHttpUpdate.update(client, updateUrl);
     // Or:
-    //t_httpUpdate_return ret = httpUpdate.update(client, "server", 80, "/file.bin");
+    //t_httpUpdate_return ret = ethHttpUpdate.update(client, "server", 80, "/file.bin");
 
     switch (ret) {
       case HTTP_UPDATE_FAILED:
-        Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+        Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", ethHttpUpdate.getLastError(), ethHttpUpdate.getLastErrorString().c_str());
         break;
 
       case HTTP_UPDATE_NO_UPDATES:
