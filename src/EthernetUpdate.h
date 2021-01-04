1a2a4a57783fdb01f36e6d06c26306a9542c2694
Steven Lian <steven.lian@gmail.com>
2021-01-04 14:43:59 +0800

    first commit

---
+++ src/EthernetUpdate.h
@@ -0,0 +1,105 @@
+/**
+ *
+ * @file HTTPUpdate.h based on ESP8266HTTPUpdate.h
+ * @date 16.10.2018
+ * @author Markus Sattler
+ * 
+ * @file EthernetUpdate.cpp based on HTTPUpdate.cpp
+ * @date 2020/12/24
+ * @author Steven Lian
+ *
+ * Copyright (c) 2015 Markus Sattler. All rights reserved.
+ * This file is part of the ESP32 Http Updater.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef ___ETH_HTTP_UPDATE_H___
+#define ___ETH_HTTP_UPDATE_H___
+
+#include <Arduino.h>
+//#include <WiFi.h>
+//#include <WiFiClient.h>
+//#include <WiFiUdp.h>
+#include <EthernetHTTPClient.h>
+#include <Update.h>
+
+/// note we use HTTP client errors too so we start at 100
+#define HTTP_UE_TOO_LESS_SPACE              (-100)
+#define HTTP_UE_SERVER_NOT_REPORT_SIZE      (-101)
+#define HTTP_UE_SERVER_FILE_NOT_FOUND       (-102)
+#define HTTP_UE_SERVER_FORBIDDEN            (-103)
+#define HTTP_UE_SERVER_WRONG_HTTP_CODE      (-104)
+#define HTTP_UE_SERVER_FAULTY_MD5           (-105)
+#define HTTP_UE_BIN_VERIFY_HEADER_FAILED    (-106)
+#define HTTP_UE_BIN_FOR_WRONG_FLASH         (-107)
+#define HTTP_UE_NO_PARTITION                (-108)
+
+enum EthernetUpdateResult {
+    HTTP_UPDATE_FAILED,
+    HTTP_UPDATE_NO_UPDATES,
+    HTTP_UPDATE_OK
+};
+
+typedef EthernetUpdateResult t_httpUpdate_return; // backward compatibility
+
+class EthernetUpdate
+{
+public:
+    EthernetUpdate(void);
+    EthernetUpdate(int httpClientTimeout);
+    ~EthernetUpdate(void);
+
+    void rebootOnUpdate(bool reboot)
+    {
+        _rebootOnUpdate = reboot;
+    }
+
+    void setLedPin(int ledPin = -1, uint8_t ledOn = HIGH)
+    {
+        _ledPin = ledPin;
+        _ledOn = ledOn;
+    }
+
+    t_httpUpdate_return update(EthernetClient& client, const String& url, const String& currentVersion = "");
+
+    t_httpUpdate_return update(EthernetClient& client, const String& host, uint16_t port, const String& uri = "/",
+                               const String& currentVersion = "");
+
+    t_httpUpdate_return updateSpiffs(EthernetClient& client, const String& url, const String& currentVersion = "");
+
+
+    int getLastError(void);
+    String getLastErrorString(void);
+
+protected:
+    t_httpUpdate_return handleUpdate(EthernetHTTPClient& http, const String& currentVersion, bool spiffs = false);
+    bool runUpdate(Stream& in, uint32_t size, String md5, int command = U_FLASH);
+
+    int _lastError;
+    bool _rebootOnUpdate = true;
+private:
+    int _httpClientTimeout;
+
+    int _ledPin;
+    uint8_t _ledOn;
+};
+
+#if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_HTTPUPDATE)
+extern EthernetUpdate ethHttpUpdate;
+#endif
+
+#endif /* ___ETH_HTTP_UPDATE_H___ */
